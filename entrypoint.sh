6bfd2daf5e186db830359794ea1bc3e39bc09b33
Stefan Martinov <stefan.martinov@gmail.com>
2022-10-19 13:11:09 +0200

    chore: add exi

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ echo "Generating GoTSRPC for $1"
 
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
 
-cd "$GITHUB_WORKSPACE" || exit
+cd "$GITHUB_WORKSPACE" || exit 1
 
-/usr/bin/gotsrpc "$1" > /dev/null
+/usr/bin/gotsrpc "$1" > /dev/null || exit 1
 
 git checkout go.mod go.sum > /dev/null
 
